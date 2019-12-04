24434788519c264df645b83a061a90557de4a7ef
sremedios <sam.remedios@gmail.com>
2019-12-04 12:25:06 -0600

    day4 part 2

--- day04.hs
+++ day04.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import Control.Monad
 import Data.List
 
@@ -7,17 +8,17 @@ window n xs = filter (\x -> length x == n) $ transpose $ take n $ tails xs
 adj :: String -> Bool
 adj = any (\s -> head s == last s) . window 2
 
-adjTwo :: String -> Bool
-adjTwo s = any (==True) $ fmap adj $ filter (\(a:b:c:d:_) -> a/=b&&b==c&&c/=d) $ window 4 (s ++ "  ")
+adj2 :: String -> Bool
+adj2 = any (==True) . fmap (\x -> fst x == 2) . fmap (liftA2 (,) length head) . group . sort
 
 alwaysInc :: String -> Bool
 alwaysInc = all (\s -> head s <= last s) . window 2
 
 solveP1 :: Int -> Int -> Int
-solveP1 start end = length $ filter (liftM2 (&&) adj alwaysInc . show) [start..end]
+solveP1 start end = length $ filter (liftM2 (&&) alwaysInc adj . show) [start..end]
 
 solveP2 :: Int -> Int -> Int
-solveP2 start end = length $ filter (liftM2 (&&) adjTwo alwaysInc . show) [start..end]
+solveP2 start end = length $ filter (liftM2 (&&) alwaysInc adj2 . show) [start..end]
 
 solve :: [Int] -> String
 solve ranges = unlines (a:b:[])
@@ -25,4 +26,4 @@ solve ranges = unlines (a:b:[])
           b = "part 2: " ++ show (solveP2 (head ranges) (last ranges))
 
 main :: IO()
-main = interact $ solve . map read . words 
+main = interact $ solve . map read . words
